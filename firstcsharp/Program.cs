8f21ab48a9ef6640b50dd465cc56794106e042db
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-08 16:28:18 +0700

    chore: learn data types

--- firstcsharp/Program.cs
+++ firstcsharp/Program.cs
@@ -6,10 +6,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("What is your name? ");
-            string name = Console.ReadLine();
+            Console.WriteLine("Biggest Double: {0}", Double.MaxValue);
+            Console.WriteLine("Smallest Double: {0}", Double.MinValue);
+            double dblPiVal = 3.14159265358979;
+            double dblBigNum = 3.00000000000002;
+            Console.WriteLine("DBL: PI + BigNum = {0}",
+                dblPiVal + dblBigNum);
 
-            Console.WriteLine($"Hello {name}");
+            Console.WriteLine("Biggest Float: {0}", float.MaxValue);
+            Console.WriteLine("Smallest Float: {0}", float.MinValue);
+            double fltPiVal = 3.141592F;
+            double fltBigNum = 3.000002F;
+            Console.WriteLine("FLT: PI + BigNum = {0}",
+                fltPiVal + fltBigNum);
         }
     }
 }
