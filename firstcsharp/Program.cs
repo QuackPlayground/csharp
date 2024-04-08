3a5f6dc0df3818b18106bad7d1e8a3ad79c7273b
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-08 19:08:38 +0700

    chore: learn string functions

--- firstcsharp/Program.cs
+++ firstcsharp/Program.cs
@@ -6,10 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Currency: {0:c}", 23.455); // $23.45
-            Console.WriteLine("Pad with 0s: {0:d4}", 23); // d4 = 4 spaces -> 0023
-            Console.WriteLine("3 Decimals: {0:f3}", 23.45555); // 23.456
-            Console.WriteLine("Commas: {0:n4}", 2300); // 2,300.0000
+            string randString = "This is a string";
+            Console.WriteLine("String Length: {0}", randString.Length);
+            Console.WriteLine("String Contains is: {0}", randString.Contains("is"));
+            Console.WriteLine("Index of is: {0}", randString.IndexOf("is"));
+            Console.WriteLine("Remove String: {0}", randString.Remove(10, 6));
+            Console.WriteLine("Insert String: {0}", randString.Insert(10, "short")); // insert in index 10 -> word "short"
+            Console.WriteLine("Replace String: {0}", randString.Replace("string", "sentence")); // replace "string" with "sentence"
+            Console.WriteLine("Compare A to B: {0}", String.Compare("A", "B", StringComparison.OrdinalIgnoreCase)); // dengan mengabaikan perbedaan huruf besar-kecil (case-insensitive).
+
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("A = a: {0}", String.Equals("A", "a", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Pad Left: {0}", randString.PadLeft(20, '.'));
+            Console.WriteLine("Pad Right: {0}", randString.PadRight(20, '.'));
+            Console.WriteLine("Trim : {0}", randString.Trim());
+            Console.WriteLine("Uppercase: {0}", randString.ToUpper());
+            Console.WriteLine("Lowercase: {0}", randString.ToLower());
         }
     }
 }
