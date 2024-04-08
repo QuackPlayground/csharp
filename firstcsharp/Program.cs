fe36ed8616381795e4463168162f9ea7678890ae
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-08 19:44:51 +0700

    chore: learn md array and for loops

--- firstcsharp/Program.cs
+++ firstcsharp/Program.cs
@@ -18,6 +18,30 @@ namespace ConsoleApp1
 
             Console.WriteLine("randomArray 0: {0}", randomArray[0].GetType());
             Console.WriteLine("Array Size: {0}", randomArray.Length);
+
+            for (int j = 0; j < randomArray.Length; j++)
+            {
+                Console.WriteLine("Array: {0}, Value: {1}", j, randomArray[j]);
+            }
+
+            Console.WriteLine("------------------");
+
+            // multidimensional array
+            string[,] custNames = new string[2, 2] { { "Bob", "Smaith" }, { "Sally", "Smith" } };
+
+            Console.WriteLine("MD Value: {0}", custNames.GetValue(0, 1));
+
+            for (int j = 0; j < custNames.GetLength(0); j++)
+            {
+                for (int k = 0; k < custNames.GetLength(0); k++)
+                {
+                    Console.WriteLine("{0} ", custNames[j, k]);
+                }
+                Console.WriteLine();
+            }
+            
+
+            
         }
     }
 }
