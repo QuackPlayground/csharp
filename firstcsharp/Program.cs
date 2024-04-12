e2a2fdcb59e77d51d7a7de08071f33828558e666
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-12 12:01:58 +0700

    chore: learn out parameter

--- firstcsharp/Program.cs
+++ firstcsharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using Microsoft.CodeAnalysis;
+using System;
+using System.ComponentModel.Design;
 using System.Globalization;
 using System.Text; // import namespace system
 
@@ -11,24 +14,19 @@ namespace ConsoleApp1
         // <Access Specifier> <Return Type> <Method Name> (Parameters)
         // { <Body> } 
 
-        static double GetSum(double x = 1, double y = 1)
+        static void DoubleIt(int x, out int solution)
         {
-            double temp = x;
-            x = y;
-            y = temp;
-            return x + y;
+            solution = x * 2;
         }
-        
         
 
         // -- END OF FUNCTIONS
 
         static void Main(string[] args)
         {
-            double x = 5;
-            double y = 4;
-            Console.WriteLine("5 + 4 = {0}", GetSum(x, y));
-            Console.WriteLine("x = {0}", x);
+            int solution;
+            DoubleIt(15, out solution);
+            Console.WriteLine("15 * 2 = {0}", solution);
         }
     }
 }
