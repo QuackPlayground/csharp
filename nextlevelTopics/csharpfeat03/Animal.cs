97e44e373075d925982f6d8b6223c32897f6a0f8
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-14 22:25:05 +0700

    chore: learn LINQ part 3

--- nextlevelTopics/csharpfeat03/Animal.cs
+++ nextlevelTopics/csharpfeat03/Animal.cs
@@ -9,10 +9,23 @@ namespace csharpfeat03
     class Animal
     {
         public string Name {  get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int AnimalID {  get; set; }
 
-        public Animal(string name = "No Name")
+        public Animal(string name = "No Name",
+            double weight=0,
+            double height=0)
         {
             Name = name;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString() 
+        {
+            return string.Format("{0} weights {1}lbs and is {2} inches tall",
+                Name, Weight, Height);
         }
     }
 }
--- nextlevelTopics/csharpfeat03/Program.cs
+++ nextlevelTopics/csharpfeat03/Program.cs
@@ -1,43 +1,29 @@
 ﻿using System;
 using System.Linq;
 using System.Xml.Linq;
+using System.Collections;
 
 namespace csharpfeat03
 {
     public class Program
     {
-
-        static int[] QueryIntArray()
-        {
-            int[] nums = {5, 10, 15, 20, 25, 30, 35};
-
-            var gt20 = from num in nums
-                       where num > 20
-                       orderby num
-                       select num;
-
-            foreach(int num in gt20) Console.WriteLine(num);
-
-            Console.WriteLine($"Get Type: {gt20.GetType()}");
-
-            Console.WriteLine();
-
-            var listGT20 = gt20.ToList<int>();
-            var arrayGT20 = gt20.ToArray();
-
-            nums[0] = 40;
-            foreach (int num in gt20) Console.WriteLine(num);
-
-            Console.WriteLine() ;
-
-            return arrayGT20;
-        }
-
         static void Main(string[] args)
         {
-            int[] intArray = QueryIntArray();
+            ArrayList famAnimals = new ArrayList()
+            {
+                new Animal {Name="Heidi", Height= .8, Weight=18},
+                new Animal {Name="Shrek", Height=4, Weight=130},
+                new Animal {Name="Congo", Height=3.8, Weight=90}
+            };
 
-            foreach (int num in intArray) Console.WriteLine(num);
+            var famAnimalEnum = famAnimals.OfType<Animal>();
+
+            var smAnimals = from animal in famAnimalEnum
+                            where animal.Weight <= 90
+                            orderby animal.Name
+                            select animal;
+            
+            foreach(var animal in smAnimals ) Console.WriteLine("{0} weights {1}lbs", animal.Name, animal.Weight);
         }
     }
 }
