b44de5922ce2a50c84723f466e954d31f0b42c9e
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-14 22:13:08 +0700

    chore: learn LINQ part 2

--- nextlevelTopics/csharpfeat03/Program.cs
+++ nextlevelTopics/csharpfeat03/Program.cs
@@ -6,17 +6,38 @@ namespace csharpfeat03
 {
     public class Program
     {
+
+        static int[] QueryIntArray()
+        {
+            int[] nums = {5, 10, 15, 20, 25, 30, 35};
+
+            var gt20 = from num in nums
+                       where num > 20
+                       orderby num
+                       select num;
+
+            foreach(int num in gt20) Console.WriteLine(num);
+
+            Console.WriteLine($"Get Type: {gt20.GetType()}");
+
+            Console.WriteLine();
+
+            var listGT20 = gt20.ToList<int>();
+            var arrayGT20 = gt20.ToArray();
+
+            nums[0] = 40;
+            foreach (int num in gt20) Console.WriteLine(num);
+
+            Console.WriteLine() ;
+
+            return arrayGT20;
+        }
+
         static void Main(string[] args)
         {
-            // Language Integrated Query (LINQ)
-            string[] dogs = { "K 9", "Brian Griffin", "Scooby Doo", "Old Yeller", "Rin Tin Tin", "Benji", "Charlie B. Barkin", "Lassie", "Snoopy" };
+            int[] intArray = QueryIntArray();
 
-            var dogSpaces = from dog in dogs
-                            where dog.Contains(" ")
-                            orderby dog descending
-                            select dog;
-
-            foreach (var i in dogSpaces) Console.WriteLine(i);
+            foreach (int num in intArray) Console.WriteLine(num);
         }
     }
 }
