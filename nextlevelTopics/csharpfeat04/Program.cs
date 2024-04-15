f21b2c1ae3aac9481ec301346bb85f3a9ba3f3f6
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-15 12:55:48 +0700

    chore: learn threads part 4

--- nextlevelTopics/csharpfeat04/Program.cs
+++ nextlevelTopics/csharpfeat04/Program.cs
@@ -12,32 +12,24 @@ namespace csharpfeat04
     public class Program
     {
 
+        static void CountTo(int maxNum)
+        {
+            for(int i = 0; i <= maxNum; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+
         static void Main(string[] args) 
         {
-            BankAcct acct = new BankAcct(10);
-            Thread[] threads = new Thread[15];
+            Thread t = new Thread(() => CountTo(10));
+            t.Start();
 
-            Thread.CurrentThread.Name = "main"; // Memberi nama thread utama (main) sebagai "main".
-
-            // Loop untuk membuat 15 thread baru.
-            for (int i = 0; i < 15; i++)
+            new Thread(() =>
             {
-                Thread t = new Thread(new ThreadStart(acct.IssueWithdraw));
-                t.Name = i.ToString(); // Memberi nama thread sesuai dengan indeksnya dalam loop.
-                threads[i] = t;
-            }
-
-            // Loop untuk memulai dan menampilkan status 15 thread yang telah dibuat.
-            for (int i = 0; i < 15; i++)
-            {
-                Console.WriteLine("Thread {0} Alive: {1}", threads[i].Name, threads[i].IsAlive); // Menampilkan status (hidup/mati) dari thread dengan nama dan status IsAlive yang sesuai.
-                threads[i].Start(); // Memulai eksekusi thread.
-
-                Console.WriteLine("Thread {0} Alive: {1}", threads[i].Name, threads[i].IsAlive); // Menampilkan status setelah thread dimulai.
-            }
-
-            Console.WriteLine("Current Priority: {0}", Thread.CurrentThread.Priority); // Menampilkan prioritas thread utama.
-            Console.WriteLine("Thread {0} Ending", Thread.CurrentThread.Name); // Menampilkan bahwa thread utama telah selesai.
+                CountTo(5);
+                CountTo(6);
+            }).Start();
         }
     }
 }
