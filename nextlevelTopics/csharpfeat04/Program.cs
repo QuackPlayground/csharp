c2a95786959576530a95e81aa909f17dc1d5b9cf
pockypoem <jeremyas.cornelis@gmail.com>
2024-04-15 11:57:50 +0700

    chore: learn threads part 1

--- nextlevelTopics/csharpfeat04/Program.cs
+++ nextlevelTopics/csharpfeat04/Program.cs
@@ -11,9 +11,24 @@ namespace csharpfeat04
 {
     public class Program
     {
+
+        static void Print1()
+        {
+            for (int i = 0; i < 1000; i++) 
+            {
+                Console.Write(1);
+            }
+        }
+
         static void Main(string[] args) 
-        { 
-            // Hello
+        {
+            Thread t = new Thread(Print1);
+            t.Start(); // try to print thousand of 1
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
         }
     }
 }
